b8ad95c39295ab312af1a4876e8c3848efe28184
Max Frederiksen <max@colesmedia.se>
2020-01-13 16:15:54 +0100

    more changes

--- source/php/Module/Comments/views/comments.blade.php
+++ source/php/Module/Comments/views/comments.blade.php
@@ -2,29 +2,34 @@
 <section class="start-page-section section-comments">
     <div class="container">
         <div class="grid grid--columns" data-equal-container>
-            <div class="u-flex grid-xs-12 grid-sm-6 grid-md-6 grid-lg-6">
-                @foreach($comments as $comment)
+            @foreach($comments as $comment)
+                <div class="u-flex grid-xs-12 grid-sm-12 grid-md-6 grid-lg-6">
                     <a href="{{ get_comment_link($comment->comment_ID) }}" class="comment-card">
-                        <div class="comment-card-icon"></div>
+                        <div class="comment-card-icon">
+                            <i class="icon-quote"></i>
+                        </div>
+
+                        
+
                         <div class="comment-card-body">
                             <h2 class="comment-card-title">
                                 {{ get_the_title($comment->comment_post_ID) }}
                             </h2>
                             <div class="small-text">
                                 <span class="author">
-                                    {{ get_post_meta($comment->comment_post_ID)['name_of_council_or_politician'][0] }}, 
+                                    {{ get_user_meta($comment->user_id)['name_of_council_or_politician'][0] }}, 
                                 </span>
                                 <time class="date">
-                                    {{ explode(' ', get_post($comment->comment_post_ID)->post_date)[0] }}
+                                    {{ explode(' ', $comment->comment_date)[0] }}
                                 </time>
                             </div>
                             <p class="card-comment-content">
-                                {{ $comment->comment_content }}
+                                {{ substr($comment->comment_content, 0, 240) . '...' }}
                             </p>
                         </div>
                     </a>
-                @endforeach
-            </div>
+                </div>
+            @endforeach
         </div>
     </div>
 </section>
